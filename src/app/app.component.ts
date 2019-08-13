0a103f49cad1617ee999b107c1a84d8574b1e1f0
sbaibos <sbaibos@gmail.com>
2019-08-13 11:16:13 +0300

    add selection,multiple selection, get selected rows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,11 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	@ViewChild('agGrid') agGrid: AgGridAngular;
   title = 'angularGrid';
 
   columnDefs = [
-    {headerName: 'Make', field: 'make', sortable: true, filter: true },
+    {headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true },
     {headerName: 'Model', field: 'model', sortable: true, filter: true },
     {headerName: 'Price', field: 'price', sortable: true, filter: true}
 ];
@@ -25,4 +27,12 @@ constructor(private http: HttpClient) {
 ngOnInit() {
     this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
 }
+
+getSelectedRows() {
+        const selectedNodes = this.agGrid.api.getSelectedNodes();
+        const selectedData = selectedNodes.map( node => node.data );
+        const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+        alert(`Selected nodes: ${selectedDataStringPresentation}`);
+    }
+
 }
