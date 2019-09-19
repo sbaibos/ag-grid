0fb2971b8f83b6664cfeae94955a390735dd172a
sbaibos <sbaibos@gmail.com>
2019-09-19 21:19:15 +0300

    added dynamic date from tutorial branch fixed some errors

--- src/app/components/stock-exchange-live/stock-exchange-live.component.ts
+++ src/app/components/stock-exchange-live/stock-exchange-live.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
  import { AgGridAngular } from 'ag-grid-angular';
 import 'ag-grid-enterprise/chartsModule';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-stock-exchange-live',
@@ -22,7 +23,8 @@ export class StockExchangeLiveComponent implements OnInit {
   private rowData: any[];
 	@ViewChild('agGrid') agGrid: AgGridAngular;
   title = 'Stock Exchange';
-  
+  currentDate = new Date();
+  myUtcDate: Date;
  
 
   
@@ -81,14 +83,90 @@ this.defaultColDef = { resizable: true };
 	
 
 }
-
+z;
+timer;
 ngOnInit() {
-    
-   
+    this.myUtcDate = new Date(Date.UTC(
+      this.currentDate.getFullYear(),
+      this.currentDate.getMonth(),
+      this.currentDate.getDate(),
+      this.currentDate.getHours(),
+      this.currentDate.getMinutes(),
+      this.currentDate.getSeconds()
+    ));
 	//this.rowData = this.http.get('https://api.iextrading.com/1.0/tops');
 	
-	
+	this.nasdaq(this.UsaTimeHours);
+	this.testFunction(this.z);
 }
+
+ usaTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  UsaTimeHours = new Date(this.usaTime).getHours();
+   UsaTimeMinutes =new Date(this.usaTime).getMinutes();
+  UsaTimeSeconds =new Date(this.usaTime).getSeconds();
+
+nasdaqCloseHours = (this.UsaTimeHours - 16) * -1 ;
+nasdaqOpenHours = (24 + 9 -this.UsaTimeHours );
+nasdaqOpenHours2 = (9-this.UsaTimeHours);
+
+
+nasdaqMinutes = (60 - this.UsaTimeMinutes)  ;
+nasdaqSeconds = (60 - this.UsaTimeSeconds ) ;
+
+testFunction(z){
+
+  let x: number = 10, y = 20;
+  let z: string;
+
+  if (x > y) 
+  {
+      console.log('x is greater than y.');
+	  
+  } 
+  else if (x < y)
+  {
+      console.log('x is less than y.'); //This will be executed
+	  this.z= "x is less than y."
+	  return this.z;
+  }
+  else if (x == y) 
+  {
+      console.log('x is equal to y');
+  }
+
+
+}
+
+nasdaq(UsaTimeHours){
+
+  
+
+  if (9 <= this.UsaTimeHours && this.UsaTimeHours<= 16) 
+  {
+      
+	  this.timer =  "NASDAQ will close in " + this.nasdaqCloseHours +  " hours  " + this.nasdaqMinutes + " minutes " + this.nasdaqSeconds + " seconds"; 
+	  console.log(this.timer);
+	  return this.timer;
+	  
+  } 
+   if (17 <= this.UsaTimeHours && this.UsaTimeHours <= 24)
+  {
+      
+	  this.timer =  "NASDAQ will open in " + this.nasdaqOpenHours  + " hours  " + this.nasdaqMinutes + " minutes" + this.nasdaqSeconds + " seconds" ; 
+	  console.log(this.timer);
+	  return this.timer;
+  }
+   if (1 <= this.UsaTimeHours && this.UsaTimeHours <= 8) 
+  {
+      
+	  this.timer =  "NASDAQ will open in " + this.nasdaqOpenHours2  + " hours  " + this.nasdaqMinutes + " minutes " + this.nasdaqSeconds + " seconds"; 
+	  console.log(this.timer);
+	  return this.timer;
+  }
+
+
+}
+
 
 onGridReady(params) {
     this.gridApi = params.api;
